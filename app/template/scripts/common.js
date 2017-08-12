bc3bbb1748874842bdf4923060c39ab8dcd23500
e5ash <e5ash.bro@gmail.com>
2017-08-13 01:02:48 +0300

    Final

--- app/template/scripts/common.js
+++ app/template/scripts/common.js
@@ -1,5 +1,37 @@
 $(document).ready(function($) {
 
-	
+	$('.images__wrap').slick({
+		dots: true,
+		appendArrows: $('.images__arrows'),
+		prevArrow: '<button type="button" class="slick-arrow slick-prev"><i class="icon icon-arrow-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-arrow slick-next"><i class="icon icon-arrow-right"></i></button>'
+	})
 
-});
+	$('.reviews__list').slick({
+		slidesToShow: 3,
+		centerMode: true,
+		variableWidth: true,
+		prevArrow: '<button type="button" class="slick-arrow slick-prev"><i class="icon icon-arrow-left-black"></i></button>',
+		nextArrow: '<button type="button" class="slick-arrow slick-next"><i class="icon icon-arrow-right-black"></i></button>',
+		responsive: [{
+			breakpoint: 1010,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: false,
+				centerMode: false
+			}
+		}]
+	})
+
+	$('#tabs').tabs();
+
+
+	$('.mobile-btn').click(function() {
+
+		$('.nav').fadeToggle();
+	});
+
+	$(window).resize(function() {
+		$('.nav').removeAttr('style');
+	});
+});;
